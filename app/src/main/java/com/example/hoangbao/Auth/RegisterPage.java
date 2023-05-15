c48bd47693be390ab47007dde1fcec6bad39438e
KaiO277 <nghiatran1527@gmail.com>
2023-05-15 22:04:56 +0700

    KaiO

--- app/src/main/java/com/example/hoangbao/Auth/RegisterPage.java
+++ app/src/main/java/com/example/hoangbao/Auth/RegisterPage.java
@@ -96,7 +96,6 @@ public class RegisterPage extends AppCompatActivity {
                                     }
                             ));
                 }
-
             }
         });
     }
